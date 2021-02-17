c2c53adb211fc1edba9dd10ba4b51a20e532ad24
COUNILLE Alexandra <54265816+FlooneMalubo@users.noreply.github.com>
2021-02-17 11:25:29 +0100

    Ajout image exercice 6

--- app/src/main/java/com/example/devmobtp02/Exercice6.java
+++ app/src/main/java/com/example/devmobtp02/Exercice6.java
@@ -7,7 +7,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,9 +18,9 @@ public class Exercice6 extends AppCompatActivity {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private static final int SENSOR_SENSITIVITY = 5;
+    private ImageView img;
+    private static final int SENSOR_SENSITIVITY = 4;
 
-    TextView distance;
     TextView distanceText;
 
     @Override
@@ -25,8 +28,8 @@ public class Exercice6 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercice6);
 
-        distance = findViewById(R.id.distance);
         distanceText = findViewById(R.id.distanceText);
+        img = findViewById(R.id.oksign);
 
         final SensorEventListener mSensorEventListener = new SensorEventListener() {
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -35,10 +38,11 @@ public class Exercice6 extends AppCompatActivity {
                if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             if (sensorEvent.values[0] >= -SENSOR_SENSITIVITY && sensorEvent.values[0] <= SENSOR_SENSITIVITY) {
                 distanceText.setText("Proche");
+                img.setVisibility(View.VISIBLE);
             } else {
                 distanceText.setText("Loin");
+                img.setVisibility(View.GONE);
             }
-            distance.setText(Float.toString(sensorEvent.values[0]));
         }
 
             }
